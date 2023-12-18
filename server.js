589983cbc73cfae6f16f9bdc7e0615404c46413d
Mohit Yadav <my701319@gmail.com>
2023-12-18 20:23:26 +0530

    1st commit

--- server.js
+++ server.js
@@ -2,16 +2,13 @@ import { config } from "dotenv";
 config();
 import express from "express";
 import cors from "cors";
-// import jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
-import mongoose from "mongoose";
-
-import authRoutes from "./routes/auth.js";
+// import mongoose from "mongoose";
 
 const app = express();
 
-app.use(express.json()); // parse data from req.body
-app.use(cookieParser()); // parse data from req.cookies
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://0th.netlify.app"],
@@ -19,9 +16,14 @@ app.use(
   })
 );
 
-app.use("/auth", authRoutes);
-
-mongoose.connect(process.env.MONGO_URI).then(() => {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.use("/", (req, res) => {
+  res.json({ success: true, msg: "Mahadev" });
 });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+// mongoose.connect(process.env.MONGO_URI).then(() => {
+//   const port = process.env.PORT || 3000;
+//   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// });
